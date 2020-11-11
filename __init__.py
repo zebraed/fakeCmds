30f2cfc58f68975cfacdc5fa86bd89d8a7ec933b
Ryoya Otani <otani@marza.com>
2020-11-12 03:40:45 +0900

    edit

--- __init__.py
+++ __init__.py
@@ -3,6 +3,3 @@
 # vim:fenc=utf-8
 # name: __init__.py
 # Created: <2020-10-21>
-from __future__ import print_function
-from __future__ import division
-from __future__ import absolute_import
--- fakeCmds.py
+++ fakeCmds.py
@@ -4,9 +4,13 @@
 # name: faceCmds.py
 # Created: <2020-10-21>
 
+import sys
+
 import _funcs
-class fakeCmds():
+class fakeCmds(object):
     cmds = _funcs
 
 class maya(fakeCmds):
     pass
+
+sys.modules['package.module'] = fakeCmds
